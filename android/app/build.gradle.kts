3234cdebf60069619ba4a7aa2e4670308a0bd4d4
Suchang Liu <90679875+leoseventeen@users.noreply.github.com>
2026-09-02 11:27:38 +0930

    Add files via upload

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -5,7 +5,7 @@ plugins {
 }
 
 android {
-    namespace = "com.example.hpt_movement_analysis"
+    namespace = "au.edu.adelaide.hpt.hpt_player_analysis"
     compileSdk = flutter.compileSdkVersion
     ndkVersion = flutter.ndkVersion
 
@@ -16,7 +16,7 @@ android {
 
     defaultConfig {
         // TODO: Specify your own unique Application ID (https://developer.android.com/studio/build/application-id.html).
-        applicationId = "com.example.hpt_movement_analysis"
+        applicationId = "au.edu.adelaide.hpt.hpt_player_analysis"
         // You can update the following values to match your application needs.
         // For more information, see: https://flutter.dev/to/review-gradle-config.
         minSdk = flutter.minSdkVersion
---
+++ android/app/src/main/kotlin/au/edu/adelaide/hpt/hpt_player_analysis/MainActivity.kt
@@ -0,0 +1,5 @@
+package au.edu.adelaide.hpt.hpt_player_analysis
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity : FlutterActivity()
