72abf5d351489acb4e5ab96f0c771c1a688f2b9c
Wentao Wang <126471875+Tao004@users.noreply.github.com>
2026-09-24 21:07:27 +0930

    Add auth and account management
    Switch between login and analysis pages and clear the session on logout.

--- lib/app.dart
+++ lib/app.dart
@@ -7,32 +7,48 @@ import 'features/auth/views/auth_page.dart';
 import 'features/consent/views/privacy_consent_gate.dart';
 
 class HptApp extends StatefulWidget {
-  const HptApp({super.key, this.authRepository});
+  const HptApp({
+    super.key,
+    this.authRepository,
+  });
+
   final AuthRepository? authRepository;
+
   @override
   State<HptApp> createState() => _HptAppState();
 }
 
 class _HptAppState extends State<HptApp> {
   bool _signedIn = false;
+
+  void _signOut() {
+    AuthRepository.clearSession();
+
+    setState(() {
+      _signedIn = false;
+    });
+  }
+
   @override
-  Widget build(BuildContext context) => MaterialApp(
-    title: 'HPT Player Analysis',
-    debugShowCheckedModeBanner: false,
-    theme: AppTheme.light,
-    home: PrivacyConsentGate(
-      child: _signedIn
-          ? AnalysisPage(
-              onSignOut: () {
-                AuthRepository.token = null;
-                AuthRepository.userEmail = null;
-                setState(() => _signedIn = false);
-              },
-            )
-          : AuthPage(
-              repository: widget.authRepository,
-              onSignedIn: () => setState(() => _signedIn = true),
-            ),
-    ),
-  );
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'HPT Player Analysis',
+      debugShowCheckedModeBanner: false,
+      theme: AppTheme.light,
+      home: PrivacyConsentGate(
+        child: _signedIn
+            ? AnalysisPage(
+                onSignOut: _signOut,
+              )
+            : AuthPage(
+                repository: widget.authRepository,
+                onSignedIn: () {
+                  setState(() {
+                    _signedIn = true;
+                  });
+                },
+              ),
+      ),
+    );
+  }
 }
